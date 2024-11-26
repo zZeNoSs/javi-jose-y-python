57535f464c9fe323302e149977daf6c85d469151
javierRodriguez <elzenyt906@gmail.com>
2024-11-26 13:09:25 +0100

    Corregir errores encontrados durante las pruebas

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-from menu import mostrar_menu
+# main.py
+from menu import mostrar_menu, ejecutar_operacion
 
-if __name__ == "__main__":
-    opcion = mostrar_menu()
-    print(f"Opción seleccionada: {opcion}")
+def main():
+    while True:
+        opcion = mostrar_menu()
+        continuar = ejecutar_operacion(opcion)
+        if not continuar:
+            break
+
+if __name__ == '__main__':
+    main()
