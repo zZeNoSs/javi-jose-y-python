575d1de21fc5c8be86761d62adc14e73f70bdd37
Josezg31 <joselitozg32@gmail.com>
2024-11-26 12:08:06 +0100

    Implementacion de opciones del menu.py

--- menu.py
+++ menu.py
@@ -1,3 +1,6 @@
+# menu.py
+from operaciones import sumar, restar, multiplicar
+
 def mostrar_menu():
     print("1- Sumar")
     print("2- Restar")
@@ -6,3 +9,29 @@ def mostrar_menu():
     print("5- Salir")
     opcion = input("Seleccione una opción: ")
     return opcion
+
+def ejecutar_opcion(opcion):
+    if opcion == '1':
+        a = float(input("Ingrese el primer número: "))
+        b = float(input("Ingrese el segundo número: "))
+        try:
+            resultado = sumar(a, b)
+            print(f"Resultado: {resultado}")
+        except ValueError as e:
+            print(e)
+    elif opcion == '2':
+        a = float(input("Ingrese el primer número: "))
+        b = float(input("Ingrese el segundo número: "))
+        try:
+            resultado = restar(a, b)
+            print(f"Resultado: {resultado}")
+        except ValueError as e:
+            print(e)
+    elif opcion == '3':
+        a = float(input("Ingrese el primer número: "))
+        b = float(input("Ingrese el segundo número: "))
+        try:
+            resultado = multiplicar(a, b)
+            print(f"Resultado: {resultado}")
+        except ValueError as e:
+            print(e)
