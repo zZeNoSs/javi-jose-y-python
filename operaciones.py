cb7205a465bc5705d0958d8841fce07c4421927a
javierRodriguez <elzenyt906@gmail.com>
2024-11-27 11:10:15 +0100

    pequeño error arreglado

--- operaciones.py
+++ operaciones.py
@@ -36,7 +36,6 @@ def dividir(a, b):
         return resultado
     else:
         raise ValueError("Ambos parámetros deben ser int o float")
-<<<<<<< HEAD
 def factorial_iterativo(n):
     if not isinstance(n, int) or n < 0:
         raise ValueError("El valor debe ser un entero no negativo")
@@ -44,7 +43,6 @@ def factorial_iterativo(n):
     for i in range(1, n + 1):
         resultado *= i
     return resultado
-=======
 def factorial_recursivo(n):
     if not isinstance(n, int) or n < 0:
         raise ValueError("El valor debe ser un entero no negativo")
@@ -52,5 +50,4 @@ def factorial_recursivo(n):
         return 1
     else:
         return n * factorial_recursivo(n - 1)
-o
->>>>>>> feature/factorial_recursivo
+
